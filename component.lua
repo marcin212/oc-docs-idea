2fe2abcac91011330eafac23c5be3105cc62fcc8
matrix89 <Matrix89@protonmail.com>
2019-01-20 18:14:11 +0100

    add modem component

--- component.lua
+++ component.lua
@@ -1,4 +1,3 @@
-
 ---@class item
 ---@field public damage number the current damage value of the item.
 ---@field public maxDamage number the maximum damage this item can have before it breaks.
@@ -12,6 +11,7 @@ local item={}
 
 ---@class component
 ---@field public inventory_controller inventory_controller
+---@field public modem modem
 local component = {}
 
 
@@ -37,13 +37,72 @@ function inventory_controller.getStackInSlot(side, slot) end
 ---@return item nil if the slot was empty
 function inventory_controller.getStackInInternalSlot(slot) end
 
-
-
-
-
-
-
 --endregion inventory_controller
+
+--region modem
+---This component is provided by network cards. Wireless network cards behave much like normal network cards, but additionally send the message as a wireless “packet” when a strength is set.
+---The modem's address must be used for networking. It can be found through component.modem.address.
+---@class modem
+local modem = {}
+
+---Returns whether this modem is capable of sending wireless messages.
+---@return boolean true if the modem is wireless
+function modem.isWireless() end
+
+---Returns the maximum packet size for sending messages via network cards. Defaults to 8192. You can change this in the OpenComputers configuration file.
+---Every value in a message adds two bytes of overhead. (Even if there's only one value.)
+---@return number packet size
+function modem.maxPacketSize() end
+
+---Returns whether the specified “port” is currently being listened on.
+---@param port number the port to check
+---@return boolean whether the port is open
+function modem.isOpen(port) end
+
+---Opens the specified port number for listening. Messages only trigger signals when they arrive on a port that is open.
+---@param port number the port to open
+---@return boolean true when was already opened
+function modem.open(port) end
+
+---Closes the specified port.
+---@param port number port to close.(Optional, defaults to all ports)
+---@return boolean true if ports were closed.
+function modem.close(port) end
+
+---Sends a network message to the specified address.
+---Any additional arguments are passed along as data.
+---@param address string address to send to
+---@param port number port to send to
+---@param data any
+---@return boolean true if the message was sent. This does not mean the message was received, only that it was sent. No port-sniffing for you.
+function modem.send(address, port, data) end
+
+---Sends a broadcast message. This message is delivered to all reachable network cards. Broadcast messages are not delivered to the modem that sent the message.
+---@param port number port to send to
+---@param data any
+---@return boolean true if the message was sent.
+function modem.broadcast(port, data) end
+
+---The current signal strength to apply when sending messages. Wireless network cards only.
+---@return number strength
+function modem.getStrength() end
+
+---Sets the signal strength. If this is set to a value larger than zero, sending a message will also generate a wireless message. The higher the signal strength the more energy is required to send messages, though. Wireless network cards only.
+---@param value number strength value
+---@return number
+function modem.setStrength(value) end
+
+---Gets the current wake-up message. When the network card detects the wake message (a string in the first argument of a network packet), on any port and the machine is off, the machine is started.
+---Works for robots, cases, servers, drones, and tablets. Linked Cards provide this same functionality.
+---@return string current message
+function modem.getWakeMessage() end
+
+---Sets the wake-up message to the specified string.
+---@param fuzzy boolean ignores additional trailing arguments in the network packet.
+---@return string
+function modem.setWakeMessage(message, fuzzy) end
+
+--endregion modem
 
 
 return component
