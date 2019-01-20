be210eaba5c52a18208f14aa699c7dab85e883af
marcin212 <marcin212@protonmail.com>
2019-01-20 18:55:17 +0100

    add component docs

--- component.lua
+++ component.lua
@@ -9,14 +9,101 @@
 ---@field public hasTag boolean whether or not the item has an NBT tag associated with it.
 local item={}
 
+---@class baseComponent: table
+local baseComponent = {}
+
 ---@class component
 ---@field public inventory_controller inventory_controller
 ---@field public modem modem
 local component = {}
 
+--region component
+
+---Returns the documentation string for the method with the specified name of the component with the specified address, if any.
+---Note that you can also get this string by using tostring on a method in a proxy, for example tostring(component.screen.isOn).
+---@param address string component address.
+---@param method string method name.
+---@return string the documentation string.
+function component.doc(address, method) end
+
+---Calls the method with the specified name on the component with the specified address, passing the remaining arguments as arguments to that method.
+---Returns the result of the method call, i.e. the values returned by the method. Depending on the called method's implementation this may throw.
+---@param address string component address.
+---@param method string method name.
+---@param ... any arguments to specified method.
+---@return any the result of the method call
+function component.invoke(address, method, ...) end
+
+---Returns a table with all components currently attached to the computer, with address as a key and component type as a value.
+---It also provides iterator syntax via __call, so you can use it like so: for address, componentType in component.list() do ... end
+---If filter is set this will only return components that contain the filter string (this is not a pattern/regular expression).
+---For example, component.list("red") will return redstone components.
+---If true is passed as a second parameter, exact matching is enforced, e.g. red will not match redstone.
+---@param filter string component filter (Optional)
+---@param exact boolean If true is passed, exact matching is enforce (Optional)
+---@return function iterator
+function component.list(filter, exact) end
+
+---Returns a table with the names of all methods provided by the component with the specified address.
+---The names are the keys in the table, the values indicate whether the method is called directly or not.
+---@param address string component address
+---@return table<string,boolean> table with the names of all methods provided by the component
+function component.methods(address) end
+
+
+---Gets a 'proxy' object for a component that provides all methods the component provides as fields, so they can be called more directly (instead of via invoke).
+---This is what's used to generate 'primaries' of the individual component types, i.e. what you get via component.blah.
+---For example, you can use it like so: component.proxy(component.list("redstone")()).getInput(sides.north), which gets you a proxy for the first redstone component returned by the component.list iterator, and then calls getInput on it.
+---Note that proxies will always have at least two fields, type with the component's type name, and address with the component's address.
+---@param address string component address
+---@return baseComponent component
+function component.proxy(address) end
+
+---Get the component type of the component with the specified address.
+---@param address string component address
+---@return string component type
+function component.type(address) end
+
+---Return slot number which the component is installed into. Returns -1 if it doesn't otherwise make sense.
+---@param address string component address
+---@return string slot number which the component is installed into
+function component.slot(address) end
+
+---Undocumented
+---@param address string component address
+---@return string
+function component.fields(address) end
+
+---Tries to resolve an abbreviated address to a full address.
+---Returns the full address on success, or nil and an error message otherwise.
+---Optionally filters by component type.
+---@param address string component address
+---@param componentType string component type (Optional)
+---@return string, string the full address on success, or nil and an error message otherwise.
+function component.get(address, componentType) end
+
+---Checks if there is a primary component of the specified component type.
+---@param componentType string component type
+---@return boolean
+function component.isAvailable(componentType) end
+
+---Gets the proxy for the primary component of the specified type.
+---Throws an error if there is no primary component of the specified type.
+---@param componentType string component type
+---@return baseComponent  primary component of the specified type or throw error
+function component.getPrimary(componentType) end
+
+---Sets a new primary component for the specified component type.
+---The address may be abbreviated, but must be valid if it is not nil.
+---Triggers the component_unavailable and component_available signals if set to nil or a new value, respectively.
+---@param componentType string component type
+---@param address string component address
+function component.setPrimary(componentType, address) end
+
+--endregion component
 
 --region inventory_controller
----@class inventory_controller
+---@class inventory_controller: baseComponent
 local inventory_controller = {}
 
 ---Returns the size of the inventory at the specified side.
@@ -95,7 +182,7 @@ function inventory_controller.getSlotStackSize(side, slot) end
 --region modem
 ---This component is provided by network cards. Wireless network cards behave much like normal network cards, but additionally send the message as a wireless “packet” when a strength is set.
 ---The modem's address must be used for networking. It can be found through component.modem.address.
----@class modem
+---@class modem: baseComponent
 local modem = {}
 
 ---Returns whether this modem is capable of sending wireless messages.
