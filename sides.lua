cffcb541fd97bff42a93fc976217c294e47e0d62
marcin212 <marcin212@protonmail.com>
2019-01-20 18:14:34 +0100

    add sides docs

---
+++ sides.lua
@@ -0,0 +1,30 @@
+
+---@class sides
+---Global table to allow you to refer to sides / directions by name
+---@field public bottom number value=0
+---@field public top number value=1
+---@field public back number value=2
+---@field public front number value=3
+---@field public right number value=4
+---@field public left number value=5
+---
+---@field public down number value=0
+---@field public negy number value=0
+---
+---@field public up number value=1
+---@field public posy number value=1
+---
+---@field public north number value=2
+---@field public negz number value=2
+---
+---@field public south number value=3
+---@field public posz number value=3
+---@field public forward number value=3
+---
+---@field public west number value=4
+---@field public negx number value=4
+---
+---@field public east number value=5
+---@field public posx number value=5
+local sides={}
+return sides
